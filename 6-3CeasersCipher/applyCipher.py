897033287e0788a14a249d7b717906f896255d5b
LPS Student <lps@lps-1011PX.(none)>
2016-05-16 15:43:01 -0700

    PS8

---
+++ 6-3CeasersCipher/applyCipher.py
@@ -0,0 +1,20 @@
+#applyCipher.py
+#A program to encrypt/decrypt user text
+#using Ceasar's Cipher
+#
+#Author: rc.rodriguez.ivan [at] leadps.org
+
+#makes a mapping of encoded alphabet to decoded alphabet 
+#arguments: key
+#returns: dictionary of mapped letters
+def createDictionary(key):
+	#place holder
+	return{}
+#gets the encrypted message from the user
+#arguments: message
+#returns: encoded message
+def getMessage():
+
+
+
+
