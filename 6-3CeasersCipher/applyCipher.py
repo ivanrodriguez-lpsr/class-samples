080b26de467ffe8eb7ecede930efd286548e109c
LPS Student <lps@lps-1011PX.(none)>
2016-06-10 14:04:45 -0700

    PS9

--- 6-3CeasersCipher/applyCipher.py
+++ 6-3CeasersCipher/applyCipher.py
@@ -11,22 +11,27 @@ import string
 #arguments: key
 #returns: dictionary of mapped letters
 def createDictionary(key):
+	# creates both alphabet we need, upper case and lowercase
 	alphabet = string.ascii_lowercase
 	UpperAlpha = string.ascii_uppercase
-	print(UpperAlpha)
+	# creates the dictionary we need
 	Dictionary = {}
-        count = 0
+	# these two loops append all the letters in the alphabets to the dictionary that we will use
 	for i in range(0, len(alphabet)):
 		Dictionary[UpperAlpha[(i + key) % 26]] = UpperAlpha[i] 	
 	for i in range(0, len(alphabet)):
 		Dictionary[alphabet[(i + key) % 26]] = alphabet[i]
-	Dictionary[" "] = " "
+	# returns the complete dictionary
+	# appends the other characters by running a loop
+	for i in range(32, 64):
+		Dictionary[chr(i)] = chr(i)
 	return Dictionary
 
 #gets the encrypted message from the user
 #arguments: none
 #returns: encoded message
 def getMessage():
+	# ask the user what they want to have encoded, gets their message, and then returns it
 	print("What message do you want to encode?")
 	message = raw_input()
 	return message	
@@ -36,20 +41,25 @@ def getMessage():
 #arguments: decoded message
 #returns: none
 def decodeMessage(message, dictionary):
+	#Creates a blank variabe so that we can input the word.
 	newMessage = ""
+	#This loop looks at the letter in the message we received, looks at it from our own dictionary, then it appends it to the blank variable for the secret message
 	for l in message:
 		newMessage = newMessage + dictionary[l] 
-		
+	# returns the secret word
 	return newMessage
 #outputs the decoded message to the terminal
 #arguments: decoded message
 #returns: none
 def printMessage(message):
+	# prints the message
 	print(message)
 
 #  execution starts here
 
 # ask for the key and message
+
+# this try wll allow the whole code to run and if it cannot run the code it will return an error message
 try:
 	print("What key would you like to use to decode?")
 	key = int(raw_input())
@@ -59,5 +69,7 @@ try:
 	decodedMessage = decodeMessage(encodedMessage, dictionary)
 	print("Okay, here is your secret message.")
 	printMessage(decodedMessage)
+
+# returns the message if there is an error in the encoding or if it's not possible
 except: 
 	print("Sorry this could not be done.")
